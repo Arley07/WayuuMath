77f33bcd34d924d8da23e9365efb5c9b718ca7fc
Arley07 <eamelop@ufpso.edu.co>
2023-05-19 18:19:15 -0500

    Activando funcion del boton Jugar

--- app/src/main/java/com/example/wayuumath/MainActivity.java
+++ app/src/main/java/com/example/wayuumath/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.wayuumath;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText et_nombre;
@@ -44,5 +48,35 @@ public class MainActivity extends AppCompatActivity {
             id = getResources().getIdentifier("manzana", "drawable", getPackageName());
             iv_personaje.setImageResource(id);
         }
+
+        mp = MediaPlayer.create(this, R.raw.alphabet_song);
+        mp.start();
+        mp.setLooping(true);
+    }
+
+    public void Jugar(View view) {
+        String nombre = et_nombre.getText().toString();
+
+        if(!nombre.equals("")){
+            mp.stop();
+            mp.release();
+
+            Intent intent = new Intent(this, Main2Activity_Nivel1.class);
+
+            intent.putExtra("jugador", nombre);
+            startActivity(intent);
+            finish();
+        } else {
+            Toast.makeText(this, "Para iniciar, priemro agrega tu nombre", Toast.LENGTH_SHORT).show();
+
+            et_nombre.requestFocus();
+            InputMethodManager imm = (InputMethodManager)getSystemService(this.INPUT_METHOD_SERVICE);
+            imm.showSoftInput(et_nombre, InputMethodManager.SHOW_IMPLICIT);
+        }
+    }
+
+    @Override
+    public void onBackPressed(){
+
     }
 }
