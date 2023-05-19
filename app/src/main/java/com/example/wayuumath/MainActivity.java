7967e210e93f1a2b2a14ae916e2f12f4f2a07dba
Arley07 <eamelop@ufpso.edu.co>
2023-05-19 17:17:50 -0500

    Prueba Git

--- app/src/main/java/com/example/wayuumath/MainActivity.java
+++ app/src/main/java/com/example/wayuumath/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.wayuumath;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText et_nombre;
+    private ImageView iv_personaje;
+    private TextView tv_bestScore;
+    private MediaPlayer mp;
+
+    int num_aleatorio = (int) (Math.random() * 10);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_nombre = (EditText) findViewById(R.id.txt_nombre);
+        iv_personaje = (ImageView) findViewById(R.id.imageView_Personaje);
+        tv_bestScore = (TextView) findViewById(R.id.textView_BestScore);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+
+        int id;
+        if (num_aleatorio == 0 || num_aleatorio == 10 || num_aleatorio == 5) {
+            id = getResources().getIdentifier("lapiz", "drawable", getPackageName());
+            iv_personaje.setImageResource(id);
+        } else if (num_aleatorio == 1 || num_aleatorio == 9) {
+            id = getResources().getIdentifier("zorro", "drawable", getPackageName());
+            iv_personaje.setImageResource(id);
+        } else if (num_aleatorio == 2 || num_aleatorio == 8) {
+            id = getResources().getIdentifier("ardilla", "drawable", getPackageName());
+            iv_personaje.setImageResource(id);
+        } else if (num_aleatorio == 3 || num_aleatorio == 7) {
+            id = getResources().getIdentifier("fresa", "drawable", getPackageName());
+            iv_personaje.setImageResource(id);
+        } else if (num_aleatorio == 4 || num_aleatorio == 6) {
+            id = getResources().getIdentifier("manzana", "drawable", getPackageName());
+            iv_personaje.setImageResource(id);
+        }
     }
 }
