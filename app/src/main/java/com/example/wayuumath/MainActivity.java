eb8dd1735cb01085b1708d0d02aeb5df1382ea21
Arley07 <eamelop@ufpso.edu.co>
2023-05-19 22:43:42 -0500

    Dar funcionamiento al boton Comprobar

--- app/src/main/java/com/example/wayuumath/MainActivity.java
+++ app/src/main/java/com/example/wayuumath/MainActivity.java
@@ -5,6 +5,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
